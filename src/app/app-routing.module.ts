034689ecac6304e778a2b8abd9d2f98550cdfb89
gilberto <emigil3010@gmail.com>
2024-11-22 13:14:57 -0600

    Card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsTableComponent } from './home/cards-table/cards-table.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: '' , component:CardsTableComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
+  
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+   
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsTableComponent } from './home/cards-table/cards-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsTableComponent
   ],
   imports: [
     BrowserModule,
